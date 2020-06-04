8fe3a2fa5b81ddd71f87f0c8120de6202e9c151b
clovisdanielcosta <58163677+clovisdanielcosta@users.noreply.github.com>
2020-06-04 04:22:06 -0300

    Criação, estilização e lógica da coleta de ítens do formulário

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -18,21 +18,24 @@ populateUFs()
 
 
 function getCities(event) {
-    const citySelect = document.querySelector("select[name=city]")
-    const stateInput = document.querySelector("input[name=city]")
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=city]")
     
     const ufValue = event.target.value
     
-    const indexOfSlectedState = event.target.selectedIndex
-    stateInput.value = event.target.options[indexOfSlectedState].text 
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text 
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
     
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
     fetch(url)
     .then( res => res.json() )
     .then( cities => {
         for ( const city of cities ) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
         
         citySelect.disabled = false 
@@ -43,4 +46,55 @@ function getCities(event) {
 document
 .querySelector("select[name=uf]")
 .addEventListener("change", getCities)
+
+/* ìtens da Coleta
+    Pegando todos os ítens */
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    /* Adicionando ouvidor de e eventos e passando uma função */
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items")
+
+let selectedItems = []
+
+function  handleSelectedItem(event) {
+    /* Dando um console para verificar
+    console.log(event.target.dataset.id); */
+    const itemLi = event.target
+    
+    /* Adicionar ou remover uma classe */
+    itemLi.classList.toggle("selected")
+    
+    const itemId = itemLi.dataset.id
+
+
+    // Verificar se existem itens selecionados,se sim
+    // pegar os itens selecionados
+
+    const alreadySelected = selectedItems.findIndex(item => {
+        const itemFound = item == itemId // true ou false
+        return itemFound
+    })
+
+    // Se já estuver selecionado
+    if ( alreadySelected >= 0 ) {
+        //Tira da seleção
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId // false
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+    } else {
+        // Se não estiver selecionado
+        // adicionar à seleção
+        selectedItems.push(itemId)
+    }
+    // Atualiza o input escondido para o envio do formulário
+    collectedItems.value = selectedItems
+    
+}
 
